a373861133b067fb2c89bfea1a6e0ef0f7144f2e
Leo Kim <leobkkim@gmail.com>
2023-12-12 15:20:41 +0900

    feat: set up panic handlers for test and non-test modes

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,20 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
 
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
@@ -37,7 +47,7 @@ fn test_runner(tests: &[&dyn Fn()]) {
 #[test_case]
 fn trivial_assertion() {
     serial_print!("trivial assertion... ");
-    assert_eq!(1, 1);
+    assert_eq!(0, 1);
     serial_println!("[ok]");
 }
 
